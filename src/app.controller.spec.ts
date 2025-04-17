e8455fe7ee0558af428d2b98f913ebbd38a66f53
farzeen-ali <farzeenaliarif786@gmail.com>
2025-04-17 22:17:12 +0500

    Controllers Basics

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "My Name is Farzeen Ali"', () => {
+      expect(appController.getHello()).toBe('My Name is Farzeen Ali');
     });
   });
 });
