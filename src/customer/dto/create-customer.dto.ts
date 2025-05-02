744540d076479466aac86142a7131f343fe0d88f
farzeen-ali <farzeenaliarif786@gmail.com>
2025-05-02 23:09:08 +0500

    Class Validator & Class Transformer

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,5 +1,9 @@
+import { IsInt, IsString } from "class-validator";
+
 /* eslint-disable prettier/prettier */
 export class CreateCustomerDto {
+  @IsString()
   name: string;
+  @IsInt()
   age: number;
 }
