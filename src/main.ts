420d0505ce376cabd54b5e8365dd2e77e2131bc4
farzeen-ali <farzeenaliarif786@gmail.com>
2025-05-16 23:23:22 +0500

    Hooks/Life Cycle Methods in Nest JS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }))
-
   await app.listen(process.env.PORT ?? 3000);
+  app.enableShutdownHooks();
 }
 bootstrap();
